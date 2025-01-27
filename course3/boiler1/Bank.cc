06b0edbc5164490622cb7f880290fbb9dc45730a
JG <simbachu@gmail.com>
2025-01-27 13:01:16 +0100

    Boiler 3

--- course3/boiler1/Bank.cc
+++ course3/boiler1/Bank.cc
@@ -3,7 +3,7 @@
 #include <iostream>
 #include "Bank.h"
 
-BankAccount::BankAccount( int amount ) : balance {amount} {
+BankAccount::BankAccount( int new_account_number ) : account_number {new_account_number} {
 
 }
 
@@ -21,14 +21,14 @@ const int BankAccount::get_balance() const{
     return balance;
 }
 
-const int Bank::register_account(){
+const std::shared_ptr<BankAccount> Bank::new_account(){
     std::lock_guard<std::mutex> guard_accounts(l_acc);
     int account_number = idx;
     ++idx;
-    std::shared_ptr<BankAccount> new_account = std::make_shared<BankAccount>();
-    accounts[account_number] = std::move(new_account);
+    auto new_account = std::make_shared<BankAccount>(account_number);
+    accounts.push_back(new_account);
  
-    return account_number;
+    return new_account;
 }
 
 void BankThread::client( Bank* bank){
@@ -37,41 +37,92 @@ void BankThread::client( Bank* bank){
     std::uniform_int_distribution<int> random_amount (100, 250); 
     std::uniform_int_distribution<int> d6 (1,6);
 
-    std::this_thread::sleep_for(std::chrono::nanoseconds(random_amount(gen)));
-
-    for ( auto& acc : bank->accounts ){
-        if ( d6(gen) > 4 ){
-            if ( d6(gen) > 3 ){
-                acc.second->deposit(random_amount(gen));
-            } else {
-                int withdraw_amount = random_amount(gen);
-                if (acc.second->get_balance() > withdraw_amount){
-                    acc.second->withdraw(withdraw_amount);
-                }
+    while( bank->running() ){
+        std::this_thread::sleep_for(std::chrono::nanoseconds(random_amount(gen)));
+        int first_number {0};
+        int second_number {0};
+        while (first_number == second_number){
+            first_number = d6(gen)-1;
+            second_number = d6(gen)-1;
+        }
+        if ( std::max(first_number, second_number) < bank->customers.size() ){
+            Transaction t {
+                bank->customers[first_number].get_account(),
+                bank->customers[second_number].get_account(),
+                random_amount(gen)
+            };
+            {
+                std::lock_guard<std::mutex> q_l (bank->l_acc);
+                bank->work_queue.push(t);
             }
+            bank->queue_s.notify_one();
+        }
+    }
+}
+
+void BankThread::worker( Bank* bank){
+    std::unique_lock<std::mutex> q_l(bank->l_acc);
+    while(bank->running()){    
+        bank->queue_s.wait(q_l, !bank->running());
+        auto t = bank->work_queue.front();
+        bank->work_queue.pop();
+        q_l.unlock();
+        if ( auto from = t.from.lock() ){
+            if ( t.amount < from->get_balance() ){
+                if ( auto to = t.to.lock() ){
+                    from->withdraw(t.amount);
+                    to->deposit(t.amount);
+                }
+                else {
+                    std::lock_guard<std::mutex> l (bank->l_log);
+                    bank->logger.log(); //missing recipient
+                }
+            } else {
+                std::lock_guard<std::mutex> l (bank->l_log);
+                bank->logger.log(); //missing balance
+            }
+        } else {
+                    std::lock_guard<std::mutex> l (bank->l_log);
+                    bank->logger.log(); //missing giver
         }
     }
 }
 
 void Bank::run(){
-    int last_acc {0};
-    while ( accounts.size() < 5 ){
-        last_acc = register_account();
-        accounts[last_acc]->deposit(300);
+    std::vector<std::string> c_names = {"Adam", "Bertil", "Ceasar", "David", "Erik", "Filip"};
+    std::lock_guard<std::mutex> c_g(l_acc);
+    for ( const auto name : c_names ){
+        customers.emplace_back(name);
+    }
+    for ( auto& customer : customers ){
+        customer.register_account(new_account());
+        if (auto acc = customer.get_account().lock() ){
+            acc->deposit(300);
+            }
     }
 
+    while ( workers.size() < 4 ){
+        workers.push_back(std::thread(BankThread::worker, this));
+    }
 
     while ( clients.size() < 10 ){
         clients.push_back(std::thread(BankThread::client, this));
     }
+
+}
+
+const bool Bank::running() const{
+    return !done;
 }
 
 void Bank::stop(){
+    done = true;
+
     for ( auto& c : clients ){
         c.join();
     }
 
     for ( const auto& acc : accounts ){
-        std::cout << "Account #" << acc.first << " balance: " << acc.second->get_balance() << '\n';
+        std::cout << "Account #" << acc->get_account_number() << " balance: " << acc->get_balance() << '\n';
     }
 }
--- course3/boiler1/Bank.h
+++ course3/boiler1/Bank.h
@@ -6,37 +6,79 @@
 #include <mutex>
 #include <thread>
 #include <memory>
+#include <chrono>
 
 class BankAccount {
     private:
+    int account_number = 0;
     int balance = 0;
     std::mutex l;
 
     public:
     BankAccount() = default;
-    BankAccount( int amount );
+    BankAccount( int account_number );
     void deposit ( int amount );
     void withdraw ( int amount );
     const int get_balance() const;
+    const int get_account_number() const;
+};
+
+class Customer {
+    std::string name;
+    std::shared_ptr<BankAccount> account;
+
+    public:
+    Customer( std::string name );
+    void register_account ( std::shared_ptr<BankAccount> );
+    std::weak_ptr<BankAccount> get_account();
+
+};
+
+struct Transaction {
+    std::weak_ptr<BankAccount> from;
+    std::weak_ptr<BankAccount> to;
+    const int amount;
+    std::chrono::time_point<std::chrono::system_clock> when = std::chrono::system_clock::now();
+};
+
+
+struct LogMessage{
+    
+};
+
+class Reporter{
+    public:
+    void log();
 };
 
 class Bank {
+    private:
+    bool done = false;
+
     public:
+    Reporter logger;
+    std::mutex l_log;
     std::atomic<int> idx = 100;
-    std::map<int, std::shared_ptr<BankAccount>> accounts;
+    std::vector<Customer> customers;
+    std::vector<std::shared_ptr<BankAccount>> accounts;
     std::mutex l_acc;
     std::vector<std::thread> clients;
+    std::vector<std::thread> workers;
+    std::condition_variable queue_s;
+    std::queue<Transaction> work_queue;
 
     Bank() = default;
-    const int register_account (  );
-    const BankAccount& get_account( int acc_number ) const;
+    const std::shared_ptr<BankAccount> new_account (  );
+    const BankAccount& get_account ( int acc_number ) const;
+    const bool running() const;
 
     void run();
     void stop();
 };
 
 namespace BankThread{
-    void client(Bank* bank);
+    void client( Bank* bank );
+    void worker( Bank* bank );
 };
 
 #endif
