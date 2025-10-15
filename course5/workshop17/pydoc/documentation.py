f11c2192cbfc14c4ce50e081151af03fcfbf48a6
JG <simbachu@gmail.com>
2025-10-15 10:00:00 +0200

    Fix pydoc output

--- course5/workshop17/pydoc/documentation.py
+++ course5/workshop17/pydoc/documentation.py
@@ -1,21 +1,22 @@
 def take_any(options):
+    """
+    Returns one of the options, if you are indecisive
+    Note:
+      Your CPU branch predictor will love you for this
+    Parameters:
+      options: list of any
+    Returns:
+      any
+    Example:
+      --> take_any([1, 2, 3])
+        2
+      --> take_any(['a', 'b', 'c'])
+        'b'
+      --> take_any([True, False])
+        True
+    """
     import random
-    """
-    # Returns one of the options, if you are indecisive
-    # Note:
-    #   Your CPU branch predictor will love you for this
-    # Parameters:
-    #   options: list of any
-    # Returns:
-    #   any
-    # Example:
-    #   >>> take_any([1, 2, 3])
-    #     2
-    #   >>> take_any(['a', 'b', 'c'])
-    #     'b'
-    #   >>> take_any([True, False])
-    #     True
-    """
+
     return random.choice(options)
 
 def ascending(a, b):
@@ -31,29 +32,29 @@ def ascending(a, b):
 
 def descending(a, b):
     """
-    # Sort function that returns True if a should come before b (descending order)
-    # Parameters:
-    #   a: first element
-    #   b: second element
-    # Returns:
-    #   bool: True if a >= b, False otherwise
+    Sort function that returns True if a should come before b (descending order)
+    Parameters:
+      a: first element
+      b: second element
+    Returns:
+      bool: True if a >= b, False otherwise
     """
     return a >= b
 
 def fun_sort(list, additional_sort_functions=[ascending, descending, take_any]):
     """
-    # Can't pick a way to sort a list?
-    # Let's take any.
-    # Uses a list of sort function, then takes any from the list.
-    # By default, it picks between acending, descending and iterated take_any.
-    # Parameters:
-    #   list: list of any
-    #   additional_sort_function: function that takes two arguments and returns a boolean
-    # Returns:
-    #   list
-    # Example:
-    #   >>> fun_sort([3, 1, 4, 1, 5])
-    #     [1, 1, 3, 4, 5]  # or [5, 4, 3, 1, 1] or some random arrangement
+    Can't pick a way to sort a list?
+    Let's take any.
+    Uses a list of sort function, then takes any from the list.
+    By default, it picks between acending, descending and iterated take_any.
+    Parameters:
+      list: list of any
+      additional_sort_function: function that takes two arguments and returns a boolean
+    Returns:
+      list
+    Example:
+      --> fun_sort([3, 1, 4, 1, 5])
+        [1, 1, 3, 4, 5]  # or [5, 4, 3, 1, 1] or some random arrangement
     """
     # Choose a random sort function
     chosen_sort = take_any(additional_sort_functions)
